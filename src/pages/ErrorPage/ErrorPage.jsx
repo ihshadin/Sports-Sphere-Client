79104ed5001341440d9473244435c989a1bd1dc6
IH Shadin <shadinfr@yahoo.com>
2023-06-08 15:08:50 +0600

    404 page added

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
     return (
         <section className='h-screen flex flex-col items-center justify-center'>
             <h2 className='text-3xl md:text-5xl font-playfair font-semibold leading-tight'>Page Not Found</h2>
-            <img className='mx-auto w-full max-w-4xl' src={image404} alt="" />
+            <img className='mx-auto w-full max-w-4xl' src={image404} alt="404 Image" />
             <Link to='/' className='text-center text-xl font-semibold uppercase inline-block py-3 px-8 sphere-primary-bg sphere-secondary'>Back to Home</Link>
         </section>
     );
