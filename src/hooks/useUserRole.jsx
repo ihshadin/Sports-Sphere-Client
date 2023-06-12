9ed7ed7d5c9a223200188eb729b8e8a3f48b1187
IH Shadin <shadinfr@yahoo.com>
2023-06-12 20:22:04 +0600

    fix problem

--- src/hooks/useUserRole.jsx
+++ src/hooks/useUserRole.jsx
@@ -12,6 +12,7 @@ const useUserRole = () => {
         enabled: !loading,
         queryFn: async () => {
             const res = await axiosSecure.get(`/users/role/${user?.email}`)
+            console.log(res);
             return res.data;
         }
     })
